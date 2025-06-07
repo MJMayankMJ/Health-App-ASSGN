ed50f2266c17a69be0d96c880735cd22b59dfe12
Mayank Jangid <226301131@gkv.ac.in>
2025-06-07 16:26:11 +0530

    Initial Commit

---
+++ Health App ASSGN/Health_App_ASSGNApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_App_ASSGNApp.swift
+//  Health App ASSGN
+//
+//  Created by Mayank Jangid on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Health_App_ASSGNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
